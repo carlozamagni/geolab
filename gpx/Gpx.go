63396fa274a4163a2d507ea0776acde04a53ddc8
Carlo Zamagni <carlozamagni@gmail.com>
2016-07-24 17:13:44 +0200

    gofmt

--- gpx/Gpx.go
+++ gpx/Gpx.go
@@ -1,23 +1,23 @@
 package gpx
 
-type(
-	Trkpt struct{
-		Lat float32	`xml:"lat,attr"`
-		Lon float32	`xml:"lon,attr"`
-		Time string 	`xml:"time"`
-		Ele float32 	`xml:"ele"`
+type (
+	Trkpt struct {
+		Lat  float32 `xml:"lat,attr"`
+		Lon  float32 `xml:"lon,attr"`
+		Time string  `xml:"time"`
+		Ele  float32 `xml:"ele"`
 	}
 
 	Trkseg struct {
-		Trkpt []Trkpt	`xml:"trkpt"`
+		Trkpt []Trkpt `xml:"trkpt"`
 	}
 
 	Trk struct {
-		Name string	`xml:"name"`
+		Name   string   `xml:"name"`
 		Trkseg []Trkseg `xml:"trkseg"`
 	}
 
-	Gpx struct{
+	Gpx struct {
 		Trk *Trk `xml:"trk"`
 	}
 )
